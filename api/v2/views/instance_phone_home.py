df6dd2fe52d069f799e5beb8b1e31ba911cd7151
John Xu <zhxu73@email.arizona.edu>
2020-05-13 11:15:16 -0700

    phone_home endpoint set instance status to 'active'

--- api/v2/views/instance_phone_home.py
+++ api/v2/views/instance_phone_home.py
@@ -7,6 +7,7 @@ from api.v2.serializers.post import InstanceSerializer as POST_InstanceSerialize
 
 from core.exceptions import ProviderNotActive
 from core.models import GroupMembership, Instance, Identity, UserAllocationSource, Project, AllocationSource
+from core.models.instance import find_instance
 from core.models.boot_script import _save_scripts_to_instance
 from core.models.instance import find_instance
 from core.models.instance_action import InstanceAction
@@ -22,6 +23,7 @@ from service.tasks.driver import destroy_instance
 from service.instance import (
     launch_instance, run_instance_action, update_instance_metadata
 )
+#from service.tasks.driver import update_metadata
 from threepio import logger, status_logger
 # Things that go bump
 from api.v2.exceptions import (
@@ -38,7 +40,6 @@ from service.exceptions import (
     VolumeMountConflict, InstanceDoesNotExist
 )
 
-
 class InstancePhoneHomeView(APIView):
     """
     Mock endpoint for phone_home request from cloud-init.
@@ -47,12 +48,59 @@ class InstancePhoneHomeView(APIView):
         """
         Instance notify Atmosphere that the cloud-init script has finished executing
         """
+
         now_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         status_logger.debug(
             "{}, {}".format(now_time, request.data)
         )
-        logger.debug(
-            "{}, {}".format(now_time, request.data)
-        )
+        logger.debug("client IP: {}".format(self._get_client_ip(request)))
+
+        try:
+            instance_id = request.data["instance_id"]
+            self._set_instance_active(instance_id)
+        except KeyError as e:
+            logger.exception(
+                "Missing key in POST request payload. {}".format(e.message)
+            )
+            return failure_response(status.HTTP_400_BAD_REQUEST, str(e.message))
+
         return Response("atmo phone_home received")
 
+    def _set_instance_active(self, instance_id):
+        """
+        Set the status of the instance to active
+        """
+        instance = find_instance(instance_id)
+        try:
+            status_update = {'tmp_status': ''}
+            update_instance_metadata(instance, status_update)
+            logger.debug("Set instance status to active")
+        except Exception as exc:
+            logger.exception("Error occurred updating instance status")
+            return Response(exc.message, status=status.HTTP_409_CONFLICT)
+
+    def _check_instance_status(self, instance_id):
+        """
+        Check if the instance is waiting for phone_home request
+        """
+        pass
+
+    def _check_client_ip(self, instance_id, ip_addr):
+        """
+        Check if the request is sent from the said instance (instance id from payload)
+        """
+        pass
+
+    def _get_client_ip(self, request):
+        """
+        Fetch the ip of client
+        """
+        try:
+            x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+            if x_forwarded_for:
+                ip = x_forwarded_for.split(',')[0]
+            else:
+                ip = request.META.get('REMOTE_ADDR')
+            return ip
+        except KeyError:
+            return None
